a64d931900d74be46a28b5f10b4cc0a641db233e
Ryan McNeil <ryan@journeyapps.com>
2020-06-20 19:50:50 -0600

    MySQL & Sequelize

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -35,19 +39,30 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
+  // .sync( { force: true})
   .sync()
   .then(result => {
     return User.findByPk(1)
   })
   .then(user => {
     if (!user) {
-      User.create({name: 'Max', email: 'test@mail.com' })
+      return User.create({name: 'Max', email: 'test@mail.com' })
     }
     return Promise.resolve(user);
   })
   .then(user => {
+    return user.createCart();
+  })
+  .then(cart => {
     // console.log(user);
     app.listen(3000);
   })
