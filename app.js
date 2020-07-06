2224c4efdccc30b64aa2fc0c4d41ad92556927c5
Ryan McNeil <ryan@journeyapps.com>
2020-07-06 11:17:47 -0600

    sessions and cookies

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
 const path = require('path');
 const config = require('./config')
+const MONGODB_URI = `mongodb+srv://node_complete:${config.mongo_password}@cluster0-jcupc.mongodb.net/shop?retryWrites=true&w=majority`
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+)
 
 app.use((req, res, next) => {
-  User.findById('5ef176f37a3901b12489d4db')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  if (req.session.user) {
+    User.findById(req.session.user._id)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  } else {
+    next();
+
+  }
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
-mongoose.connect(`mongodb+srv://node_complete:${config.mongo_password}@cluster0-jcupc.mongodb.net/shop?retryWrites=true&w=majority`)
+mongoose.connect(MONGODB_URI)
   .then( () => {
     User.findOne().then(user => {
       if (!user) {
---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get('Cookie').trim().split('=')[1] === 'true';
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5ef176f37a3901b12489d4db')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err =>{
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+}
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy( err => {
+    console.log(err);
+    res.redirect('/');
+  })
+}
