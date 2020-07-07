4c366ba26c690a6de82fd769b50ca8936cf131a4
Ryan McNeil <ryan@journeyapps.com>
2020-07-06 23:30:02 -0600

    advanced auth

--- controllers/admin.js
+++ controllers/admin.js
@@ -61,21 +61,25 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/');
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
       product.imageUrl = updatedImageUrl;
       return product.save()
-    })
-    .then(() => {
-      console.log('UPDATED PRODUCT!');
-      res.redirect('/admin/products');
+        .then(() => {
+          console.log('UPDATED PRODUCT!');
+          res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({userId: req.user._id})
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -88,7 +92,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id})
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const config = require('../config')
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport =require('nodemailer-sendgrid-transport');
@@ -109,4 +110,102 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect('/');
   })
+}
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  message = message.length > 0 ? message[0] : null;
+
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password', 
+    errorMessage: message
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  const email = req.body.email;
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({email: email})
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then( result => {
+        res.redirect('/');
+        return transporter.sendMail({
+          to: email,
+          from: 'ryan.mcneil288@gmail.com',
+          subject: 'Password Reset',
+          html: `
+          <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to reset a new password.</p>
+          `
+        })
+      })
+      .then( () => {
+        console.log('Mail successfully sent to', email);
+      })
+      .catch(err => console.log(err));
+  })
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+    .then(user => {
+      let message = req.flash('error');
+      message = message.length > 0 ? message[0] : null;
+      if (!user) {
+
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password', 
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      })
+
+    })
+    .catch(err => console.log(err))
+
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+  .then(user => {
+    resetUser = user;
+    return bcrypt.hash(newPassword, 12);
+  })
+  .then(hashedPassword => {
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = null;
+    resetUser.resetTokenExpiration = null;
+
+    return resetUser.save();
+  })
+  .then( () => {
+    res.redirect('/login');
+  })
+  .catch(err => console.log(err));
 }
